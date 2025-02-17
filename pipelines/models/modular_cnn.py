dd03d89854acd44a7a1b316a3ccff5203034db4a
Luis Salomon Flores Ugalde <vf1barukiri@gmail.com>
2025-02-17 13:13:32 +0900

    Avance 4 update

--- pipelines/models/modular_cnn.py
+++ pipelines/models/modular_cnn.py
@@ -20,3 +20,25 @@ class CustomCNN(nn.Module):
         x = self.conv2(x)
         x = self.flatten(x)
         return self.fc(x)
+
+
+
+class CustomCNN_3(nn.Module):
+    def __init__(self, in_channels=2, channel1=32, channel2=64, channel3=128, out_features=8, img_size=(512, 512)):
+        super().__init__()
+        self.conv1 = CNN_custom(in_channels, channel1, channel1)
+        self.conv2 = CNN_custom(channel1, channel2, channel2)
+        self.conv3 = CNN_custom(channel2, channel2, channel3)
+        self.flatten = nn.Flatten()
+
+        # Compute final flattened size
+        flattened_size = channel3 * (img_size[0] // 8) * (img_size[1] // 8)  # Since each CNN_custom does 2x2 MaxPool
+
+        self.fc = nn.Linear(flattened_size, out_features)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flatten(x)
+        return self.fc(x)
